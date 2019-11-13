18e57c96f0e4b76e4ede6999970b7716b7713624
sgsj <992678704@qq.com>
2019-11-13 17:35:05 +0800

    完善引力计算

--- js/home.js
+++ js/home.js
@@ -19,33 +19,30 @@ window.onload=function(){
         let e = event || window.event;
         mouse.x = e.clientX;
         mouse.y = e.clientY;
-        console.log(e.clientX,e.clientY);
+        mouse.mass = 10000;
+        mouse.radius = 100;
     }
 
     var cxt = an.getContext('2d');
 
     function draw(){
-        cxt.clearRect(0,0,an.width,an.height);
-        console.log("1");
         for(var i=0;i<dotarry.length;i++){
             cxt.fillStyle="#000000";
             cxt.beginPath();
             cxt.arc(dotarry[i].x+dotarry[i].speedX,dotarry[i].y+dotarry[i].speedY,dotarry[i].radius,0,Math.PI*2,true);
             cxt.closePath();
             cxt.fill();
-            
-            console.log(dotarry[i].x);
 
             dotarry[i].x += dotarry[i].speedX;
             dotarry[i].y += dotarry[i].speedY;
 
-            for (let j = i; j < dotarry.length; j++) {
-                if( Math.abs(dotarry[i].x - dotarry[j].x)<100 && Math.abs(dotarry[i].y - dotarry[j].y)<100 ){
-                    cxt.moveTo(dotarry[i].x,dotarry[i].y);
-                    cxt.lineTo(dotarry[j].x,dotarry[j].y);
-                    cxt.stroke();
-                };
-            }
+            // for (let j = i; j < dotarry.length; j++) {
+            //     if( Math.abs(dotarry[i].x - dotarry[j].x)<100 && Math.abs(dotarry[i].y - dotarry[j].y)<100 ){
+            //         cxt.moveTo(dotarry[i].x,dotarry[i].y);
+            //         cxt.lineTo(dotarry[j].x,dotarry[j].y);
+            //         cxt.stroke();
+            //     };
+            // }
 
             if( dotarry[i].x > an.width || dotarry[i].x < 0 || dotarry[i].y > an.height || dotarry[i].y < 0){
                 dotarry[i] = {
@@ -68,17 +65,33 @@ window.onload=function(){
     };
 
     function gravitation(mos) {
+        console.log(Math.floor(Math.random()));
+        if(mos.x == undefined && mos.y == undefined) return;
         for (let i = 0; i < dotarry.length; i++) {
-            let mouseX = dotarry[i].x - mos.x;
-            let mouseY = dotarry[i].y - mos.y
-            if( Math.abs(mouseX) <= 50 && Math.abs(mouseY) <= 50 ){
-                dotarry[i].speedX -= mouseX;
-                dotarry[i].speedY -= mouseY;
-            }
+            let dX = dotarry[i].x - mos.x;
+            let dY = dotarry[i].y - mos.y;
+            let distQ = dX*dX + dY*dY;
+            let dist = Math.sqrt(distQ);
+
+            let F = (mos.mass * dotarry[i].radius) / distQ; //引力公式
+
+            let ax = F * dX/dist; //作用力作用于加速度
+            let ay = F * dY/dist;
+
+            dotarry[i].speedX -= ax / dotarry[i].radius;
+            dotarry[i].speedY -= ay / dotarry[i].radius;
+
+            // if( Math.abs(mouseX) <= 50 && Math.abs(mouseY) <= 50 ){
+            //     dotarry[i].speedX = parseInt(mouseX/dotarry[i].speedX);
+            //     dotarry[i].speedY = parseInt(mouseY/dotarry[i].speedY);
+
+            //     console.log('Xspeed:',dotarry[i].speedX,'Yspeed:',dotarry[i].speedY);
+            // }
         }
     }
 
     this.setInterval( function(){
+        cxt.clearRect(0,0,an.width,an.height);
         draw()
         gravitation(mouse)
     },30);
